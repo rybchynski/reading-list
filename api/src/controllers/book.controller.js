ee6b124162effaf05880165ab01d72d44feeab28
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-10-02 20:19:33 +0300

    API: add relations to book queries

--- api/src/controllers/book.controller.js
+++ api/src/controllers/book.controller.js
@@ -1,6 +1,11 @@
 const mainCrud = require('./main.controller');
 const { Book } = require('../model');
 
+const relations = {
+  getAll: ['categories', 'authors'],
+  get: ['category', 'author'],
+};
+
 module.exports = {
-  ...mainCrud(Book),
+  ...mainCrud(Book, relations),
 };
