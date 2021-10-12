febc212b3b357c6a19b2cce59e114dde5dd0a393
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-10-12 18:56:19 +0300

    WIP: frontend app. Create table and card components for books view

---
+++ app/src/components/books/CardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Books Card view</h2>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from "vuex";
+  export default {
+    computed: {
+      ...mapGetters(["books"]),
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
---
+++ app/src/components/books/TableView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <table-component
+      :columns="columns"
+      :actions="actions"
+      :data="books"
+      :withTotal="true"
+      @onEdit="handleEdit"
+      @onView="handleView"
+    ></table-component>
+  </div>
+</template>
+
+<script>
+  import TableComponent from "@/components/table/TableComponent";
+  import { columns, actions, config } from "./setup";
+
+  export default {
+    components: {
+      TableComponent,
+    },
+    props: {
+      books: {
+        type: Array,
+        default: () => [],
+      },
+    },
+    data: () => ({
+      columns,
+      actions,
+      config,
+    }),
+    methods: {
+      handleEdit({ id }) {
+        this.$router.push(`/books/${id}/edit`);
+      },
+      handleView({ id }) {
+        this.$router.push(`/books/${id}`);
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
