805fa3ea48c2d4eb07d63983f641bd3146f9c055
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-10-23 12:59:06 +0300

    feat(author-actions): create a new author methods
    implement create/update author functionality. Update author store

--- app/src/services/author.service.js
+++ app/src/services/author.service.js
@@ -2,5 +2,7 @@ import { request } from "./main.service";
 
 const getAuthor = (id) => request({ url: `authors/${id}`, method: "get" });
 const getAuthors = () => request({ url: `authors`, method: "get" });
+const updateAuthor = (id) => request({ url: `authors/${id}`, method: 'put' })
+const createAuthor = () => request({ url: `authors`, method: "post" })
 
-export { getAuthor, getAuthors };
+export { getAuthor, getAuthors, updateAuthor, createAuthor };
