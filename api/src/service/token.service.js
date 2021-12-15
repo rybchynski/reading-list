7f1ba56cb27a05ac6833096142fa534fe3320279
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-12-15 09:27:31 +0200

    feat(api-user): create user token and mail services

---
+++ api/src/service/token.service.js
@@ -0,0 +1,63 @@
+const jwt = require('jsonwebtoken');
+const token = require('../model/Token');
+
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: '15m',
+    });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: '5h',
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await token.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+
+      return tokenData.save();
+    }
+    const t = await token.create({ user: userId, refreshToken });
+
+    return t;
+  }
+
+  async removeToken(refreshToken) {
+    const tokenData = await token.deleteOne({ refreshToken });
+
+    return tokenData;
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await token.findOne({ refreshToken });
+
+    return tokenData;
+  }
+}
+
+module.exports = new TokenService();
---
+++ api/src/service/user.service.js
@@ -0,0 +1,101 @@
+const { User } = require('../model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail.service');
+const tokenService = require('./token.service');
+const ApiErrorsHandler = require('../exceptions/api.errors.handler');
+
+class UserService {
+  #getUserData(user) {
+    return {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      isActivated: user.isActivated,
+    };
+  }
+
+  async registration(username, email, password) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw ApiErrorsHandler.BadRequest(
+        `User with email '${email}' already exist.`,
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/v1/users/activate/${activationLink}`,
+    );
+
+    const userData = this.#getUserData(user);
+    const tokens = tokenService.generateTokens({ ...userData });
+    await tokenService.saveToken(userData.id, tokens.refreshToken);
+
+    return { ...tokens, user: userData };
+  }
+
+  async login(email, password) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw ApiErrorsHandler.BadRequest('User with this email not fould.');
+    }
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+    if (!isPassCorrect) {
+      throw ApiErrorsHandler.BadRequest('Wrong email or password.');
+    }
+    const userData = this.#getUserData(user);
+    const tokens = tokenService.generateTokens({ ...userData });
+    await tokenService.saveToken(userData.id, tokens.refreshToken);
+
+    return { ...tokens, user: userData };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiErrorsHandler.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const savedToken = await tokenService.findToken(refreshToken);
+    if (!userData || !savedToken) {
+      throw ApiErrorsHandler.UnauthorizedError();
+    }
+    const user = await User.findById(userData.id);
+    const userInfo = this.#getUserData(user);
+    const tokens = tokenService.generateTokens({ ...userInfo });
+    await tokenService.saveToken(userInfo.id, tokens.refreshToken);
+
+    return { ...tokens, user: userInfo };
+  }
+
+  async getAllUsers() {
+    const users = await User.find();
+
+    return users;
+  }
+
+  async activate(activationLink) {
+    const user = await User.findOne({ activationLink });
+    if (!user) {
+      throw ApiErrorsHandler.BadRequest('Incorrect activation link');
+    }
+    user.isActivated = true;
+    await user.save();
+  }
+}
+
+module.exports = new UserService();
