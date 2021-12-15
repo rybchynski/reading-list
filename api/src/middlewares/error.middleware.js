2dec8008a8e10cf7f909f65eb62591b62f3a5565
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-12-15 09:57:25 +0200

    feat(api-user): create error and auth middlewares, update main index file

---
+++ api/src/middlewares/error.middleware.js
@@ -0,0 +1,12 @@
+const ApiErrorsHandler = require('../exceptions/api.errors.handler');
+
+module.exports = function (err, req, res, next) {
+  console.log(err);
+  if (err instanceof ApiErrorsHandler) {
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
+  }
+
+  return res.status(500).json({ message: 'Something went wrong...' });
+};
