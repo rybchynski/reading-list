9ecc5f9aeb929a625854a2748ed54ecbefef04ae
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-12-15 09:53:17 +0200

    feat(api-user): create user controller

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,9 +1,11 @@
 const author = require('./author.controller');
 const category = require('./category.controller');
 const book = require('./book.controller');
+const user = require('./user.controller');
 
 module.exports = {
   author,
   category,
   book,
+  user,
 };
