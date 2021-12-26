afbde147b9b805396130f4aa8d6a75170a479fe0
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-12-26 17:39:18 +0200

    feat(app-registration): add needed routes, create route guard

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,95 +1,134 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "../store";
 
 Vue.use(VueRouter);
+
+function authGuard(to, from, next) {
+  if (store.getters.isAuth) {
+    next({ name: "Home" });
+  } else {
+    next();
+  }
+}
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
   },
   {
     path: "/books/add",
     name: "AddBook",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/books/form/index.vue"
+        /* webpackChunkName: "AddBook" */ "@/components/books/form/index.vue"
       ),
   },
   {
     path: "/books/:id",
     name: "ViewBook",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
-      import(/* webpackChunkName: "home" */ "@/components/books/ViewBook.vue"),
+      import(
+        /* webpackChunkName: "ViewBook" */ "@/components/books/ViewBook.vue"
+      ),
   },
   {
     path: "/books/:id/edit",
     name: "EditBook",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/books/form/index.vue"
+        /* webpackChunkName: "EditBook" */ "@/components/books/form/index.vue"
       ),
   },
   // User routes.
   {
     path: "/profile",
     name: "Profile",
-    meta: { layout: "pure" },
+    meta: {
+      layout: "pure",
+      shouldBeAuth: true,
+    },
     component: () =>
-      import(/* webpackChunkName: "home" */ "../views/user/Profile.vue"),
+      import(/* webpackChunkName: "Profile" */ "../views/user/Profile.vue"),
   },
   {
     path: "/login",
     name: "Login",
     meta: { layout: "pure" },
+    beforeEnter: authGuard,
     component: () =>
-      import(/* webpackChunkName: "home" */ "../views/user/Login.vue"),
+      import(/* webpackChunkName: "Login" */ "../views/user/Login.vue"),
   },
   {
     path: "/sign-up",
     name: "SignUp",
     meta: { layout: "pure" },
     component: () =>
-      import(/* webpackChunkName: "home" */ "../views/user/SignUp.vue"),
+      import(/* webpackChunkName: "SignUp" */ "../views/user/SignUp.vue"),
   },
   /////////////////////////////////////////////////////////////////////////////
 
   {
     path: "/categories",
     name: "Categories",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/categories/Categories.vue"
+        /* webpackChunkName: "Categories" */ "@/components/categories/Categories.vue"
       ),
   },
   {
     path: "/categories/:id/edit",
     name: "EditCategory",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/categories/form/index.vue"
+        /* webpackChunkName: "EditCategory" */ "@/components/categories/form/index.vue"
       ),
   },
   {
     path: "/categories/add",
     name: "AddCategory",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/categories/form/index.vue"
+        /* webpackChunkName: "AddCategory" */ "@/components/categories/form/index.vue"
       ),
   },
   {
     path: "/authors",
     name: "Authors",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
         /* webpackChunkName: "authors" */ "@/components/authors/Authors.vue"
@@ -98,28 +137,48 @@ const routes = [
   {
     path: "/authors/add",
     name: "AddAuthor",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "authors" */ "@/components/authors/form/index.vue"
+        /* webpackChunkName: "AddAuthor" */ "@/components/authors/form/index.vue"
       ),
   },
   {
     path: "/authors/:id/edit",
     name: "EditAuthor",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/authors/form/index.vue"
+        /* webpackChunkName: "EditAuthor" */ "@/components/authors/form/index.vue"
       ),
   },
   {
     path: "/authors/:id",
     name: "ViewAuthor",
-    meta: { layout: "main" },
+    meta: {
+      layout: "main",
+      shouldBeAuth: true,
+    },
     component: () =>
       import(
-        /* webpackChunkName: "home" */ "@/components/authors/ViewAuthor.vue"
+        /* webpackChunkName: "ViewAuthor" */ "@/components/authors/ViewAuthor.vue"
+      ),
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    meta: {
+      layout: "pure",
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "NotFound" */ "@/components/app/NotFound.vue"
       ),
   },
 ];
@@ -128,6 +187,21 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  let { isAuth } = JSON.parse(localStorage.getItem("vuex"));
+  if (to.matched.some((item) => item.meta.shouldBeAuth)) {
+    if (!isAuth) {
+      next({
+        name: "Login",
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
