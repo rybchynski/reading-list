151b3384352fcb789df9783598338a3b58340ce0
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2021-12-30 17:42:15 +0200

    feat(app-profile): profile page

--- app/src/components/user/Profile.vue
+++ app/src/components/user/Profile.vue
@@ -1,12 +1,76 @@
 <template>
-  <div>
-    <h2>Profile page</h2>
-    <router-link to="/">Back</router-link>
+  <div class="list-page-wrapper">
+    <h1 class="center-align page_title">User Info</h1>
+    <div class="row">
+      <div class="col s12">
+        <div class="user-info row">
+          <div class="user-info__image col s12 m6">
+            <img class="responsive-img" :src="userImage" alt="logo" />
+          </div>
+          <div class="user-info__data col s12 m6">
+            <div class="user-info__data-name">
+              <span>Username: </span>{{ user.username }}
+            </div>
+            <div class="user-info__data-email">
+              <span>Email: </span>{{ user.email }}
+            </div>
+            <div class="user-info__data-created">
+              <span>Created: </span> {{ createdAt }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {};
+  import { mapGetters } from "vuex";
+  export default {
+    computed: {
+      ...mapGetters(["user"]),
+      createdAt() {
+        const date = new Date(this.user.createdAt);
+        return (
+          date.getDate() +
+          "/" +
+          (date.getMonth() + 1) +
+          "/" +
+          date.getFullYear() +
+          " " +
+          date.getHours() +
+          ":" +
+          date.getMinutes()
+        );
+      },
+    },
+    data() {
+      return {
+        userImage: require("../../assets/images/empty-logo.png"),
+      };
+    },
+  };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .user-info__image {
+    margin-bottom: 20px;
+  }
+  .user-info__data {
+    font-size: 14px;
+    font-weight: 600;
+    color: black;
+
+    span {
+      text-transform: uppercase;
+      font-size: 12px;
+      color: gray;
+      margin-right: 20px;
+    }
+
+    &-email {
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+  }
+</style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -63,7 +63,7 @@ const routes = [
     path: "/profile",
     name: "Profile",
     meta: {
-      layout: "pure",
+      layout: "main",
       shouldBeAuth: true,
     },
     component: () =>
